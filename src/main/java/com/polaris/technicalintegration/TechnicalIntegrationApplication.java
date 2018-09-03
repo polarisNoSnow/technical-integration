673bcf14db03ff0dcd3128a9edb55e7f9d9cda7f
polaris <787225863@qq.com>
2018-09-03 21:31:18 +0800

    init

---
+++ src/main/java/com/polaris/technicalintegration/TechnicalIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.polaris.technicalintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalIntegrationApplication.class, args);
+	}
+}
